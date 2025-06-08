9075d39b6551ef1c03165f29ee0bde12b1280e40
parandyk <mparandyk@interia.pl>
2025-06-08 13:27:18 +0200

    Update tasks.py
    Signed-off-by: parandyk <mparandyk@interia.pl>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1784,7 +1784,7 @@ def guess_model_scale(model_path):
         (str): The size character of the model's scale (n, s, m, l, or x).
     """
     try:
-        return re.search(r"yolo(e-)?[v]?\d+([nslmx])", Path(model_path).stem).group(2)  # noqa
+        return re.search(r"yolo(e-)?[v]?\d+([qryzafupnslmx])", Path(model_path).stem).group(2)  # noqa
     except AttributeError:
         return ""
 
