cfc6d8d9a2c5025dcd6f14934c8148137f11c021
parandyk <mparandyk@interia.pl>
2025-06-14 09:58:15 +0200

    Update block.py
    Signed-off-by: parandyk <mparandyk@interia.pl>

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -1355,12 +1355,13 @@ class Attention(nn.Module):
         B, C, H, W = x.shape
         N = H * W
         qkv = self.qkv(x)
-        print(f"q: {q}")
-        print(f"k: {k}")
-        print(f"v: {v}")
+        print(f"qkv: {qkv}")
         print(f"self.num_heads: {self.num_heads}")
         q, k, v = qkv.view(B, self.num_heads, self.key_dim * 2 + self.head_dim, N).split(
             [self.key_dim, self.key_dim, self.head_dim], dim=2
+        print(f"q: {q}")
+        print(f"k: {k}")
+        print(f"v: {v}")
         )
 
         attn = (q.transpose(-2, -1) @ k) * self.scale
